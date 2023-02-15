d8bfbe2069ea3d770a071e3efb68b0a88902cb1f
janusz <janusz.wrobel.2@gmail.com>
2023-02-15 23:34:25 +0000

    changes

--- Scripts.Test/Tests.fs
+++ Scripts.Test/Tests.fs
@@ -3,6 +3,7 @@ module Scripts.Test
 
 open System
 open System.IO
+open System.Security.Cryptography
 open NUnit.Framework
 open Scripts.Git
 open Scripts.Sample
@@ -54,18 +55,74 @@ let TestCheckouts () =
     let projFile = Path.Combine(baseDir, "src/Fantomas/Fantomas.fsproj")
     mkArgsFile projFile (Path.Combine(__SOURCE_DIRECTORY__, "fantomas.args"))
 
+type ExtractCore =
+    {
+        Mvid : string
+        DllHash : string
+        PdbHash : string
+        RefHash : string
+    }
+
+type ExtractMeta =
+    {
+        DllTimestamp : DateTime
+        Name : string
+        Directory : string
+    }
+
+type Extract =
+    {
+        Core : ExtractCore
+        Meta : ExtractMeta
+    }
+
+module Paths =
+    let mvid = "mvid.txt"
+    let dll = "out.dll"
+    let pdb = "out.pdb"
+    let ref = "ref.dll"
+    let args = "fscargs.txt"
+    let extract = "extract.json"
+
+let getFileHash (file : string) =
+    if not (File.Exists(file)) then
+        failwith $"File '{file}' does not exist"
+    use md5 = MD5.Create()
+    use stream = File.OpenRead(file)
+    let hash = md5.ComputeHash(stream)
+    BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant()
+
+let getExtract (name : string) (dir : string) =
+    let subPath name = Path.Combine(dir, name)
+    let dll = subPath Paths.dll
+    {
+        Extract.Core =
+            {
+                ExtractCore.Mvid = File.ReadAllText(subPath Paths.mvid).Trim()
+                DllHash = getFileHash dll
+                PdbHash = getFileHash (subPath Paths.pdb)
+                RefHash = getFileHash (subPath Paths.ref)
+            }
+        Extract.Meta =
+            {
+                ExtractMeta.Directory = dir
+                DllTimestamp = File.GetLastWriteTimeUtc(dll)
+                Name = name
+            }
+    }
+
 let go (outputDir : string) (projectArgs : ProjectSArgs) (fscDll : string) =
     Directory.CreateDirectory(outputDir) |> ignore
     let subPath name = Path.Combine(Environment.CurrentDirectory, Path.Combine(outputDir, name))
-    let dllPath = subPath "test.dll"
+    let dllPath = subPath Paths.dll
     let {Project = project; Args = args} = projectArgs
     let args =
         args
         |> SArgs.setOutput dllPath
-        |> SArgs.setKeyValue "--refout" (subPath "test.ref.dll")
+        |> SArgs.setKeyValue "--refout" (subPath Paths.ref)
         |> SArgs.setKeyValue "--debug" "portable"
     
-    let argsFile = subPath "args.txt"
+    let argsFile = subPath Paths.args
     args
     |> SArgs.toFile argsFile
     
@@ -76,7 +133,7 @@ let go (outputDir : string) (projectArgs : ProjectSArgs) (fscDll : string) =
         .ExecuteAssertSuccess()
     
     let mvid = MvidReader.getMvid dllPath
-    let mvidPath = subPath "mvid.txt"
+    let mvidPath = subPath Paths.mvid
     File.WriteAllText(mvidPath, mvid.ToString())
     
 [<Test>]
